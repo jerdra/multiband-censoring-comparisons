fix lombscargle interpolation method, full series is needed for variance normalization

confounds/censor_method.py
@@ -414,12 +414,11 @@ def _interpolate_frames(data: Union[Nifti1Image, npt.ArrayLike],
     t = np.arange(0, t_num_samples) * t_r
     interp_vals = lombscargle_interpolate(t=t[mask],
                                           x=sgls,
-                                          s=t[censor],
-                                          fs=1 / t_r)
+                                          s=t)
 
     res = np.empty((sgls.shape[0], t_num_samples), dtype=sgls.dtype)
     res[:, mask] = sgls
-    res[:, censor] = interp_vals
+    res[:, censor] = interp_vals[:, censor]
     res = res.reshape((*data.shape[:-1], t_num_samples))
 
     if is_nifti:

confounds/spectral_interpolation.py
@@ -144,7 +144,6 @@ def Ow(t: npt.ArrayLike, w: npt.ArrayLike) -> npt.ArrayLike:
 def lombscargle_interpolate(t: npt.ArrayLike,
                             x: npt.ArrayLike,
                             s: npt.ArrayLike,
-                            fs: float,
                             chunk_size: Optional[int] = 100) -> npt.ArrayLike:
     '''
     Perform non-uniform spectral interpolation of a time-series t
@@ -156,7 +155,6 @@ def lombscargle_interpolate(t: npt.ArrayLike,
         t: Non-uniform sampling times [T x 1]
         x: Observed values x_p(t) [P x T]
         s: Target time-series to sample to [S x 1]
-        fs: Sampling frequency
         chunk_size: Perform interpolation in chunks if X is 2 dimensional
             to limit memory consumption
 
@@ -175,9 +173,7 @@ def lombscargle_interpolate(t: npt.ArrayLike,
     o = Ow(t, w)
     cterm = CT(t, w, o)
     sterm = ST(t, w, o)
-    X = make_design(s, w)
-
-    coefs = np.empty((chunk_size, w.shape[0] * 2))
+    X = make_design(s, w, o)
 
     dc = (cterm**2).sum(axis=0)
     ds = (sterm**2).sum(axis=0)
@@ -203,4 +199,4 @@ def lombscargle_interpolate(t: npt.ArrayLike,
 
         x_i[i, :] = coefs @ X.T
 
-    return x_i
+    return normalize_variance_on(x_i, x)